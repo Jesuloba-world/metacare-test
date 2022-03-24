f1c669d2fc123870098e1e0b313645994e473070
Jesuloba <jesulobajohn@gmail.com>
2022-03-24 14:27:03 +0100

    some changes

--- src/components/main/sub/efficiency/header/header.style.js
+++ src/components/main/sub/efficiency/header/header.style.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const Container = styled.div`
 	height: 110px;
 	width: 100%;
-	padding-inline: 40px 56px;
 	display: flex;
 	align-items: center;
 	justify-content: space-between;
